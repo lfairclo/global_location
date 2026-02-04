858775f7e3e9b91131c6d4c6ce11988392dcaf10
lfairclo <137823676+lfairclo@users.noreply.github.com>
2026-02-04 14:46:38 +0000

    Change so there are no posts to api

--- js/api.js
+++ js/api.js
@@ -1,12 +1,8 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbw9JlOgwFD7jsZxG1jIeL9XCd4ECNUeeR8yEM6qKQsYAWi_dk4JPftn-2hq6Wp6_5FZdQ/exec";
 
-async function apiCall(payload) {
-  const res = await fetch(API_URL, {
-    method: "POST",
-    body: JSON.stringify(payload),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-  return res.json();
+async function apiCall(params) {
+  const query = new URLSearchParams(params).toString();
+  const response = await fetch(`${API_URL}?${query}`);
+  return await response.json();
 }
+
